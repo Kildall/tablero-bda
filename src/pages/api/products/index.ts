45f8b4b1dd55f8b1d86853d002f2717ce3623fd3
Pedro B <pebustos.135@gmail.com>
2023-09-18 20:57:57 -0300

    feat: agregado comando de prisma migrate

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,10 +1,8 @@
-import prismaExample from "@/data/test";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const rta = await prismaExample();
-  return res.status(200).json(rta);
+  return res.status(200).json("Productos");
 }
